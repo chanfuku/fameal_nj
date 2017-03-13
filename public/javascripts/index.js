80c6790bb06eaea6cb817359aae541695a073366
chanfuku <k.ebato0318@gmail.com>
2017-03-13 11:17:58 +0900

    issue#15:バリデーションエラーの場合と確認画面から戻る場合の再入力画面を作る

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,17 +1,12 @@
-var image_onclick = function (event) {
-    window.alert('Hello World !');
-};
-
 var submit = function (event) {
     window.location.href="/submit";
 };
 
 var back = function (event) {
-    window.location.href="/";
+    window.location.href="/input";
 };
 
 var document_onready = function (event) {
-    $('.img-thumbnail').on('click', image_onclick);
     $('#submit').on('click', submit);
     $('#back').on('click', back);
 };
