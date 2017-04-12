ff0fbfe0fb73855cc40a959ae63a86586cb0fcbd
chanfuku <koheiebato@gmail.com>
2017-04-12 19:57:35 +0900

    issue#6:確認画面と完了画面をパワポにある文言に修正

--- public/javascripts/service.js
+++ public/javascripts/service.js
@@ -92,11 +92,11 @@ angular.module('myApp')
                 }).
                 then(function successCallback(response) {
                     $scope.show_progress = false;
-                    $scope.result = "送信されました。担当者からご連絡致します。";
+                    $scope.show_comp_success=true;
                 }, function errorCallback(response) {
                     //通信に失敗
                     $scope.show_progress = false;
-                    $scope.result = '送信エラーが発生しました。';
+                    $scope.show_comp_failure=true;
                 });
             }
             this.compQuestion = function($scope){
@@ -114,11 +114,11 @@ angular.module('myApp')
                 }).
                 then(function successCallback(response) {
                     $scope.show_progress = false;
-                    $scope.result = "送信されました。担当者からご連絡致します。";
+                    $scope.show_comp_success=true;
                 }, function errorCallback(response) {
                     //通信に失敗
                     $scope.show_progress = false;
-                    $scope.result = '送信エラーが発生しました。';
+                    $scope.show_comp_failure=true;
                 });
             }
         }
